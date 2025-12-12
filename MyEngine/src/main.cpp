908697015d530abb60b195bfa47bc8ce7e9d9edf
EhabNasir <ehabnasir@icloud.com>
2025-12-12 20:52:57 +0000

    Added DeltaTime
    - Now have fixed time step
    - Added Running loop

--- MyEngine/src/main.cpp
+++ MyEngine/src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "MyEngine is running!\n";
-    std::cin.get();
+    using clock = chrono::high_resolution_clock;
+
+    auto lastTime = clock::now();
+
+    cout << "Engine Start\n";
+    cin.get();
+
+    bool running = true;
+
+    while(running)
+    {
+        auto now = clock::now();
+        chrono::duration<float> delta = now - lastTime;
+        lastTime = now;
+
+        float deltaTime = delta.count();
+        cout << "Delta Time: " << deltaTime << " seconds\n";
+
+        this_thread::sleep_for(chrono::milliseconds(16));
+    }
+
     return 0;
 }
